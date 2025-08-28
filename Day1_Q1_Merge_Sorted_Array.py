13fb653b60e7e6a25a01edc468ec4f6a00b6448e
ksj1999 <122510623+ksj1999@users.noreply.github.com>
2025-08-28 00:00:41 -0500

    Update Day1_Q1_Merge_Sorted_Array.py

--- Day1_Q1_Merge_Sorted_Array.py
+++ Day1_Q1_Merge_Sorted_Array.py
@@ -8,3 +8,20 @@ class Solution:
         """
         nums1 = nums1[0:m]
         nums1 = (nums1+nums2).sort()
+
+-------------------------------------------------------------------------------------------------------------
+
+# Error 2. Change to sorted(num1) but still didn't change the final output in IDE.
+# Reason: When slicing a list it automatically makes a new one. -> nums1 = nums1[sliced] makes a new copy of original list nums1 and names it nums1
+# basically all the function below just chages the new variable nums1 and never touches the original nums1 memory. 
+
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1 = nums1[0:m]
+        nums1 = (nums1+nums2)
+        print(nums1)
+        nums1 = sorted(nums1)
+        print(nums1)
