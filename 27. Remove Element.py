f647295bb4fcb57bc19c15752a5ccc7630c35f92
ksj1999 <122510623+ksj1999@users.noreply.github.com>
2025-08-28 18:56:01 -0500

    Update 27. Remove Element.py

--- 27. Remove Element.py
+++ 27. Remove Element.py
@@ -1,4 +1,4 @@
-# Try 1. Use Del list or Remove() function
+# Error 1. Use Del list or Remove() function
 # Reason : Number of list got reduced by del or remove() function
 # Note : del list / list.remove() does not make a copy like slicing does. It operates on the list itself.
 
@@ -12,6 +12,17 @@ class Solution:
         k = len(nums)
 
 
-# Try 2. Use Del without changing the index of list
-# 
+# Fix 1. Use Del without changing the index of list
+# Method : Remove from the back of the list
+# Overall bad idea..
 
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        for i in range(len(nums)-1, -1, -1):
+            if val == nums[i]:
+                print(val, nums[i])
+                del nums[i]
+                print(nums)
+        k = len(nums)
+
+# 
