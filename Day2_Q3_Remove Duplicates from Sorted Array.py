0dce611aba51c6ea826c8d64fd0a87037d03c202
ksj1999 <122510623+ksj1999@users.noreply.github.com>
2025-08-28 21:57:23 -0500

    Update Day2_Q3_Remove Duplicates from Sorted Array.py

--- Day2_Q3_Remove Duplicates from Sorted Array.py
+++ Day2_Q3_Remove Duplicates from Sorted Array.py
@@ -1,5 +1,4 @@
 # Error 1. index out of range error
-# 
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
@@ -9,3 +8,17 @@ class Solution:
                 nums[k] = nums[i]
                 k+=1
         return k
+
+# Final Solution
+# Start from 1 because first element is always unique.
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[k-1]:
+                nums[k] = nums[i]
+                k+=1
+        return k
+            
