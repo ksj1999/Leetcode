2c52a5471d5f40fea564462ac922bbc651d14e2f
ksj1999 <122510623+ksj1999@users.noreply.github.com>
2025-08-28 21:30:10 -0500

    Update and rename 27. Remove Element.py to Day2_Remove Element_Two Pointer Problem.py

--- Day2_Remove Element_Two Pointer Problem.py
+++ Day2_Remove Element_Two Pointer Problem.py
@@ -25,4 +25,26 @@ class Solution:
                 print(nums)
         k = len(nums)
 
-# 
+# Upgrade 3. Using While so that the index doesn't matter.
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        while val in nums:
+            nums.remove(val) 
+        return len(nums)
+
+
+# Final Solution. Two Pointer problem
+# This method is opposite of what I have done(focusing on when the val and num[] is same) as it focuses when the val and num[] is different.
+# Use K and I to replace whenever the value is different.
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+
+        k = 0
+        for i in range(len(nums)):
+            print(nums)
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
