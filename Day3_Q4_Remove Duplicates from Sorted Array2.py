af54ca92a49c7ab4a2a80493bf795a8b431886d4
ksj1999 <122510623+ksj1999@users.noreply.github.com>
2025-08-29 21:57:23 -0500

    Update Day3_Q4_Remove Duplicates from Sorted Array2.py

--- Day3_Q4_Remove Duplicates from Sorted Array2.py
+++ Day3_Q4_Remove Duplicates from Sorted Array2.py
@@ -81,3 +81,16 @@ class Solution:
         
         return k
 
+----------
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        
+        k = 2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[k-2]:
+                nums[k] = nums[i]
+                k+=1
+        
+        return k
