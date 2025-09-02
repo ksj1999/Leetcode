eddc1c333cef2d0be25ca2819fa907252693a72d
ksj1999 <122510623+ksj1999@users.noreply.github.com>
2025-09-01 22:09:09 -0500

    Update Day4_Q6_Rotate Array.py

--- Day4_Q6_Rotate Array.py
+++ Day4_Q6_Rotate Array.py
@@ -8,3 +8,21 @@ class Solution:
         for i in range(k):
             temp = nums.pop()
             nums.insert(0, temp)
+
+------------------------
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n 
+
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
