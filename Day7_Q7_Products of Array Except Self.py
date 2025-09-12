45f0347e78b451936bb17617d46d73361d1686bb
ksj1999 <122510623+ksj1999@users.noreply.github.com>
2025-09-11 21:29:14 -0500

    Update Day7_Q7_Products of Array Except Self.py

--- Day7_Q7_Products of Array Except Self.py
+++ Day7_Q7_Products of Array Except Self.py
@@ -13,3 +13,54 @@ class Solution:
             else:
                 temp.append(0)
         return temp
+------------------------------------
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        temp = []
+        zero_count = nums.count(0)
+
+        if zero_count > 1:
+            return [0] * len(nums)
+
+        k = 1
+        for i in nums:
+            if i != 0:
+                k *= i
+
+        if zero_count == 0:
+            for j in nums:
+                temp.append(k // j)  # integer division avoids float issues
+        else:
+            for j in nums:
+                if j == 0:
+                    temp.append(k)
+                else:
+                    temp.append(0)
+
+        return temp
+
+
+-------------------------------------------------------------------
+
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        temp = []
+
+        for i in range(len(nums)):
+            k = 1
+
+            for j in range(len(nums)):
+                if i != j:
+                    k = k*nums[j]
+
+            temp.append(k)
+        
+        return temp
+
+
+----------------------------------
+
