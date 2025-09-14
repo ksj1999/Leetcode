0f01a3a88d6317593902df56b659f70584b3af8a
ksj1999 <122510623+ksj1999@users.noreply.github.com>
2025-09-14 13:23:43 -0500

    Create Day8_Q10_Valid Palindrome.py

---
+++ Day8_Q10_Valid Palindrome.py
@@ -0,0 +1,20 @@
+# Error : case sensitive
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        temp = []
+        for i in s:
+            if 48<= ord(i) <=57 or 65<= ord(i) <=90 or 97<= ord(i) <=122:
+                temp.append(i)
+            
+        backward = temp[::-1]
+        
+        print(temp)
+        print(backward)
+        
+        if temp == backward:
+            return True
+        else:
+            return False
+
+      ----------------------------------
