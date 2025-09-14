d7b32fa5e83a291aa0d8494ec36a3f3fd6cabe49
ksj1999 <122510623+ksj1999@users.noreply.github.com>
2025-09-14 13:25:18 -0500

    Update Day8_Q10_Valid Palindrome.py

--- Day8_Q10_Valid Palindrome.py
+++ Day8_Q10_Valid Palindrome.py
@@ -18,3 +18,21 @@ class Solution:
             return False
 
       ----------------------------------
+# change all to lower case
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        temp = []
+        for i in s:
+            if 48<= ord(i) <=57 or 65<= ord(i) <=90 or 97<= ord(i) <=122:
+                temp.append(i.lower())
+            
+        backward = temp[::-1]
+
+        print(temp)
+        print(backward)
+        
+        if temp == backward:
+            return True
+        else:
+            return False
